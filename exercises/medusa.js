56aa965abc64f694eb81a8b140013f8a40414580
Brian Bower <brian@brianbower.com>
2020-01-20 18:15:42 -0700

    Add persons and medusa

---
+++ exercises/medusa.js
@@ -0,0 +1,23 @@
+class Medusa {
+
+  constructor(name){
+    this.name = name;
+    this.statues = [];
+  }
+
+  stare(victim) {
+    if (this.statues.length < 3){
+      victim.stoned = true;
+      this.statues.push(victim);
+    }
+    else if (this.statues.length >= 3) {
+      victim.stoned = true;
+      this.statues.push(victim);
+      let first_victim = this.statues.shift();
+      first_victim.stoned = false;
+    }
+  }
+
+}
+
+module.exports= Medusa;
